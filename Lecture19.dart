3d6b0af06accf96f131722d38f9c7ab0b5fe3fbb
usamajutt786999 <usamanasir786999@gmail.com>
2023-11-10 17:00:03 +0500

    Lecture 19 done Lambda functions

---
+++ Lecture19.dart
@@ -0,0 +1,13 @@
+
+void main()
+{
+  // anonymus , arrow , lambda
+  var s=()=>print("Hello");
+  s();
+  // add two numbers 
+  var addition=(a,b)=>a+b;
+  print(addition(12,23));
+  var sq=(a)=>a*a;
+  print(sq(34));
+
+}
